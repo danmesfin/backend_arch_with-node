69a6e8d2cf824967c1152f43efcc554fcc4ab13c
danmesfin <danielmsfn@gmail.com>
2022-10-28 10:26:57 +0300

    REST api

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
-console.log("hello world");
+const express = require('express');
+const colors = require('colors');
+const dotenv = require('dotenv').config();
+const { errorHandler } = require('./middleware/errorMiddleware');
+const connectDB = require('./config/db');
+const PORT = process.env.PORT;
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+app.use('/api/goals', require('./routes/goalRoutes'))
+app.use(errorHandler);
+
+app.listen(PORT, ()=> console.log(`server running @ port${PORT}`));
